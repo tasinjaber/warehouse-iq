41c0d6f47857bad3093f982aee4a09c8b3a4c926
Sami Al Jaber Tasin <tasinjaber407@gmail.com>
2026-09-19 22:31:04 +0200

    feat: complete portfolio workspace

---
+++ eslint.config.js
@@ -0,0 +1,1 @@
+import parser from"@typescript-eslint/parser";export default[{ignores:["dist"],files:["**/*.{ts,tsx}"],languageOptions:{parser,parserOptions:{ecmaVersion:"latest",sourceType:"module",ecmaFeatures:{jsx:true}}},rules:{}}];
